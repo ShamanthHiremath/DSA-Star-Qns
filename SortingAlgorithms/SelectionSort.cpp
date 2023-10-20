2bafdd3817600479b95eb6fb9b1296c936282eaf
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2023-10-20 14:14:37 +0530

    Bubble Sort added

--- SortingAlgorithms/SelectionSort.cpp
+++ SortingAlgorithms/SelectionSort.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h> 
 void selectionSort(vector<int>& arr, int n)
-{   
+{   //Pusing the lighter elements to the start
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             if(arr[i]>arr[j]){
