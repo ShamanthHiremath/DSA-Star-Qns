6b6620b44c653a384f0a3bd5879d5e556fb29dce
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2023-10-22 04:03:42 +0530

    ValidPalindrome.cpp

---
+++ Miscellaneous/DayOnGiveDate.cpp
@@ -0,0 +1,8 @@
+// 15th August, 2010 = (2009 years + Period 1.1.2010 to 15.8.2010)
+// Odd days in 1600 years = 0
+// Odd days in 400 years = 0
+// 9 years = (2 leap years + 7 ordinary years) = (2 x 2 + 7 x 1) = 11 = 4 odd days.
+// Jan. Feb. Mar.  Apr.  May.  Jun.  Jul.  Aug.
+// (31 + 28 + 31 + 30 + 31 + 30 + 31 + 15) = 227 days = (32 weeks + 3 days) = 3 odd days.
+// Total number of odd days = (0 + 0 + 4 + 3) = 7 =  0 odd days.
+// Given day 15/08/2010 is Sunday
