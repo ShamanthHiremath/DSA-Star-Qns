476611f1144fd9db36e5934fd1240682f4802397
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2023-11-30 20:44:34 +0530

    .\CheckCircularLinkedList.cpp

---
+++ LinkedList/CheckCircularLinkedList.cpp
@@ -0,0 +1,58 @@
+/*
+Floyd's Cycle Detection Algorithms
+
+The problem is that a few linked list have a cycle within them (read about cycles in linked lists on internet), so the temp pointer never actually comes back at head or at NULL causing an infinite loop
+EXAMPLE:
+No of nodes = 7
+1 2 5 4 3 8 5
+The last 5 connects to 3rd 5
+
+*/
+
+bool isCircular(Node* head){
+    if(head == NULL) return true;
+    if(head->next == NULL) return(false); 
+    Node * slow = head;
+    Node * fast = head; 
+    while(slow!=NULL && fast!=NULL){
+        slow = slow->next;
+        fast = fast->next;
+        if(fast!=NULL) fast = fast->next; 
+        if(slow == fast){
+            if(slow == head) return true;
+            else return false;
+        }
+    }
+    return false;
+}
+
+
+
+//CHECK FOR STRAIGHT LINKED LIST WHICH EITHER END AT NULL OR CONNECT BACK AT HEAD
+
+bool isCircular(Node* head){
+    if(head==NULL || head->next==NULL || head->next==head)return true;
+    Node* temp=head;
+    while(temp->next!=head){
+        if(temp->next==NULL){
+            return false;
+        }
+        temp=temp->next;
+    }
+    if(temp->next==NULL)return false;
+    if(temp->next!=head)return false;
+    return true;
+}
+bool isCircular(Node* head){
+    if(head==NULL )return true;
+    Node* temp=head->next;
+    while(temp!=head && temp!=NULL){
+        // if(temp->next==NULL){
+        //     return false;
+        // }
+        temp=temp->next;
+    }
+    //if(temp->next==NULL)return false;
+    if(temp==head)return true;
+    return false;
+}
