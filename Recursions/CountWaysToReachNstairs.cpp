9f1b643bf550589e907e7a99ef7be874ac6caf1a
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2023-12-10 00:49:04 +0530

    .\CountWaysToReachNstairs.cpp

--- Recursions/CountWaysToReachNstairs.cpp
+++ Recursions/CountWaysToReachNstairs.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h> 
+//exceeds time limits
 int countDistinctWays(int nStairs) {
     if(nStairs==1|| nStairs==2) return nStairs;
     //if(nStairs==3)return 3;
@@ -8,3 +9,42 @@ int countDistinctWays(int nStairs) {
 
     return ans;
 }
+
+//doesnot exceed time limit by searching in the map
+class Solution {
+public:
+    int climbStairs(int n) {
+        unordered_map<int, int> memo;
+        return climbStairsMemo(n, memo);
+    }
+
+private:
+    int climbStairsMemo(int n, unordered_map<int, int>& memo) {
+        if (n == 0) return 0;
+        if (n == 1|| n==2) return n;
+
+        if (memo.find(n) != memo.end()) {
+            return memo[n];
+        }
+
+        int ways = climbStairsMemo(n - 1, memo) + climbStairsMemo(n - 2, memo);
+        memo[n] = ways;
+
+        return ways;
+    }
+};
+//normal method
+class Solution {
+public:
+    int climbStairs(int n) {
+ 
+        int prev1 = 1, prev2 = 1, current = 1;
+        for(int i = 2; i <= n; i ++) {
+            current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+        return current;
+        
+    }
+};
