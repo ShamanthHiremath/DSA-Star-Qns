bac2db163c347f782d5cff5aa9b3f5fb74ead445
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2023-12-12 00:21:35 +0530

    .\HouseRobber.cpp

--- DPDynamicPrograming/HouseRobber.cpp
+++ DPDynamicPrograming/HouseRobber.cpp
@@ -18,15 +18,22 @@ Total amount you can rob = 2 + 9 + 1 = 12.
 class Solution {
 public:
     int solve(vector<int>& nums, int n, int i, vector<int>&dp ){
+        //the 0th house u rob all the money and it isnt an adjacent house
         if(i==0)return nums[0];
+        //if the street ends... ull go home richhhhh
         if(i<0)return 0;
+        //the max money robbed which if has been already stored in dp[i]i.e dp[i]!=-1(not unsaved)is returned
         if(dp[i]!=-1)return dp[i];
+        //if u choose to rob a house then u rib it and go for the net to next(no adjacents.... shushhhh) 
         int choose=nums[i]+solve(nums, n, i-2, dp);
+        //if u dont choose that house go for the next one(boommmmm)
         int notchoose=solve(nums, n, i-1, dp );
+        //store the max value of these two in dp[i], i.e max value of money robbed in that very ith house and return
         return dp[i]=max(choose, notchoose);
 
     }
     int rob(vector<int>& nums) {
+    //initialise a vector to  store the returned minvalue for each jump
         vector<int>dp(nums.size()+1, -1);
         return solve(nums, nums.size()-1, nums.size()-1, dp);
     }
