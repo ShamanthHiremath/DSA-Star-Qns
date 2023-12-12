0594b7e4d84eb0945cea3d826201236dee80c01f
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2023-12-12 23:06:08 +0530

    .\DeleteMiddleElement.cpp

--- Stacks/DeleteMiddleElement.cpp
+++ Stacks/DeleteMiddleElement.cpp
@@ -22,3 +22,19 @@ void deleteMiddle(stack<int>&inputStack, int N){
       inputStack.push(arr[i]);
    }
 }
+//RECURSIVE
+#include <bits/stdc++.h>
+void solve(stack<int>&inputStack, int N, int count){
+   if(count==(N)/2){
+      inputStack.pop();
+      return;
+   }
+   int storedtop=inputStack.top();
+   inputStack.pop();
+   solve(inputStack, N, count+1);
+   inputStack.push(storedtop);
+} 
+void deleteMiddle(stack<int>&inputStack, int N){
+   //int count=0;
+   solve(inputStack, N, 0);
+}
