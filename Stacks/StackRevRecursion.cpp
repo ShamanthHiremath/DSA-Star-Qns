340115ac7505857a209c63a3434fa4162d5aff55
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2023-12-13 21:28:57 +0530

    .\StackRevRecursion.cpp

--- Stacks/StackRevRecursion.cpp
+++ Stacks/StackRevRecursion.cpp
@@ -1,22 +1,27 @@
-void insertt(stack<int>& stack, int x){
+void insertAtBottom(stack<int>& stack, int x){
     if(stack.empty()){
         stack.push(x);
         return;
     }
     int storeprevelement=stack.top();
     stack.pop();
-    insertt(stack, x);
+    insertAtBottom(stack, x);
     stack.push(storeprevelement);
 }
 
 void reverse(stack<int> &stack, int top){
+
     if(stack.empty()){
         return;
     }
+    //RECURSION SAYS DO ONE JOB HANDLE THE REST
+    //Just store the top element and assume that the rest of the stack is reversed
     int storetop=stack.top();
     stack.pop();
+    //assumed
     reverse(stack, top);
-    insertt(stack, storetop);
+    //insert stored top element at the bottom
+    insertAtBottom(stack, storetop);
 }
 
 void reverseStack(stack<int> &stack) {
