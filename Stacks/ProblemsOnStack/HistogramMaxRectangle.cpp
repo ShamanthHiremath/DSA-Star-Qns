19ebc0a1b7dd013816f9219603130cb3f1140e65
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2023-12-16 15:58:04 +0530

    .\HistogramMaxRectangle.cpp

---
+++ Stacks/ProblemsOnStack/HistogramMaxRectangle.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int>prevsmallerindex(vector<int> heights, int n){
+        stack<int>s;
+        vector<int>ans(n);
+        s.push(-1);
+        for(int i=0; i<n; i++){
+            while(s.top()!=-1 && heights[s.top()]>=heights[i]){
+                s.pop();
+            }
+            ans[i]=s.top();
+            s.push(i);
+        }
+        return ans;
+
+    }
+    vector<int>nextsmallerindex(vector<int> heights, int n){
+        stack<int>s;
+        vector<int>ans(n);
+        s.push(-1);
+        for(int i=n-1; i>=0; i--){
+            //int store=heights[i];
+            while(s.top()!=-1 && heights[s.top()]>=heights[i]){
+                s.pop();
+            }
+            ans[i]=s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        vector<int>next(n);
+        next=nextsmallerindex(heights,n);
+        vector<int>prev(n);
+        prev=prevsmallerindex(heights,n);
+        int area=INT_MIN;
+        for(int i=0; i<n; i++){
+            int l=heights[i];
+            if(next[i]==-1){
+                next[i]=n;
+            }
+            // if(prev[i]==-1){
+            //     prev[i]=1;
+            // }
+            int b=next[i]-prev[i]-1;
+            // area=l*b;
+            area=max(area, l*b);
+        }
+        return area;
+
+    }
+};
