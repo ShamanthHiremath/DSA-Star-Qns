0fd6bbca0fdb1a73db99521daecee0a6cc111d85
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2023-12-23 21:25:03 +0530

    .\PermutationsOfaString.cpp

--- Recursions/Combinations/PermutationsOfaString.cpp
+++ Recursions/Combinations/PermutationsOfaString.cpp
@@ -23,3 +23,22 @@ public:
         
     }
 };
+
+void perm(char *list, int i,int n)
+{
+int j,temp;
+if(i==n)       {
+              for(j=0;j<=n;j++)
+                         printf("%c",list[j]);
+   printf("  ");
+   }
+   else
+   {
+   for(j=i;j<=n;j++)
+   {
+      SWAP(list[i],list[j], temp);
+      perm(list,i+1,n);
+      SWAP(list[i],list[j],temp);
+  }
+}
+}
