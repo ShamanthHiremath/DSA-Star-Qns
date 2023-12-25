84b056fd2a29e3820e0f15d7678b3ecf7ea787c8
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2023-12-25 23:39:08 +0530

    .\Reversing_A_Queue.cpp

---
+++ Queues/Reversing_A_Queue.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+    public:
+    void solve(queue<int>& q){
+        if(q.empty()){
+            return;
+        }
+        int element=q.front();
+        q.pop();
+        solve(q);
+        q.push(element);
+    }
+    queue<int> rev(queue<int> q)
+    {
+        //APPROACH 1 is to use a stack and push dequeued elements
+        //later pop the elements from stack and enqueue itin the same queue
+        // stack<int>s;
+        // while(!q.empty()){
+        //     s.push(q.front());
+        //     q.pop();
+        // }
+        // while(!s.empty()){
+        //     q.push(s.top());
+        //     s.pop();
+        // }
+        // return q;
+        solve(q);
+        return q;
+    }
+};
