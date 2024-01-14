4150fbca9169ed5555544c9f2fffb4d16fda6360
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2024-01-14 18:28:23 +0530

    .\TopologicalSortingUsingBFSKahnAlgo.cpp

--- Graphs/TopologicalSortingUsingBFSKahnAlgo.cpp
+++ Graphs/TopologicalSortingUsingBFSKahnAlgo.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h> 
 void TopoSortBFS(unordered_map<int, list<int>> &adj, vector<int> &indegrees, vector<int> &ans){
 	queue<int>q;
+    //push all the 0 indegree nodes to queue
 	for(int i = 0; i < indegrees.size(); ++i){
         if (indegrees[i] == 0) {
             q.push(i);
@@ -12,7 +13,9 @@ void TopoSortBFS(unordered_map<int, list<int>> &adj, vector<int> &indegrees, vec
 		//store the ans
 		ans.push_back(frontnode);
 		for( auto neighbor: adj[frontnode]){
+            //Decrease the no of indegrees instead of marking visited
 			indegrees[neighbor]--;
+            //if equal to 0 then push to the queue
 			if(indegrees[neighbor]==0){
 				q.push(neighbor);
 			}
@@ -22,12 +25,14 @@ void TopoSortBFS(unordered_map<int, list<int>> &adj, vector<int> &indegrees, vec
 
 vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
     unordered_map<int, list<int>>adj;
+    // To calculate the indegrees of a node
     vector<int>indegrees(v);
     //prepare adj list
     for(int i=0; i<e;i++){
         int u=edges[i][0];
         int v=edges[i][1];
         adj[u].push_back(v);
+        //counts indegrees
         indegrees[v]++;
     }
     // for(auto i: adj){
@@ -35,6 +40,7 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
 	// 		indegrees[j]++;
 	// 	}
 	// }
+    //To store ans for topo order
     vector<int> ans;
 	TopoSortBFS(adj, indegrees, ans);
 	return ans;
