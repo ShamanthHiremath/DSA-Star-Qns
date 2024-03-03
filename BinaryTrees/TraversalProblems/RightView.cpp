a99ec8c042dae815f8b56c8cb4cbd61edb1cbc20
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2024-03-04 00:00:41 +0530

    .\RightView.cpp

--- BinaryTrees/TraversalProblems/RightView.cpp
+++ BinaryTrees/TraversalProblems/RightView.cpp
@@ -2,6 +2,35 @@
 Right View: A list containing elements of right view of the binary tree.
 */
 
+
+//EASY SOLN
+
+class Solution
+{
+    public:
+    void solve(Node *root, vector<int>&ans, int lvl){
+        if(root==NULL)return;
+        if(lvl==ans.size()){
+            ans.push_back(root->data);
+        }
+        solve(root->right, ans, lvl+1);
+        solve(root->left, ans, lvl+1);
+    }
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(Node *root)
+    {
+        if(root==NULL){
+            return {};
+        }
+        
+        vector<int>ans;
+        solve(root, ans, 0);
+        
+        return ans;
+    }
+};
+
+//SAME AS PREVIOUS LOGIC
 class Solution
 {
     public:
