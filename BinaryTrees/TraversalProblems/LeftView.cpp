20ead1bc2f9876c8294b5c7532e9ee9e71bcf162
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2024-03-04 00:00:29 +0530

    .\LeftView.cpp

--- BinaryTrees/TraversalProblems/LeftView.cpp
+++ BinaryTrees/TraversalProblems/LeftView.cpp
@@ -1,6 +1,33 @@
 /*
 LeftView: A list containing elements of left view of the binary tree.
 */
+
+//EASIER APPROACH
+class Solution
+{
+    public:
+    void solve(Node *root, vector<int>&ans, int lvl){
+        if(root==NULL)return;
+        if(lvl==ans.size()){
+            ans.push_back(root->data);
+        }
+        solve(root->left, ans, lvl+1);
+        solve(root->right, ans, lvl+1);
+    }
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> leftView(Node *root)
+    {
+        if(root==NULL){
+            return {};
+        }
+        
+        vector<int>ans;
+        solve(root, ans, 0);
+        
+        return ans;
+    }
+};
+
 vector<int> leftView(Node *root)
 {
     if(root==NULL){
