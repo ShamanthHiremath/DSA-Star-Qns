4ea3a37128a0698899d94f311ed4ad0d3d11cf4d
ShamanthHiremath <shamanth.hiremath.101@gmail.com>
2024-03-04 22:56:51 +0530

    .\FlattenABinaryTreeUsingMorrisTraversal.cpp

---
+++ BinaryTrees/FlattenABinaryTreeUsingMorrisTraversal.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+    public:
+    void flatten(Node *root)
+    {
+        Node* current=root;
+        while(current!=NULL){
+            //if cuurent->left exists
+            if(current->left){
+                //Find the INORDER PREDECESSOR
+                Node* pred=current->left;
+                while(pred->right!=NULL){
+                    pred=pred->right;
+                }
+                //connect pred's right and current's right
+                pred->right=current->right;
+                //connect the right of current to its left child
+                //this eliminates double pointerism and flattens it
+                current->right=current->left;
+            }
+            //move to the right part
+            current=current->right;
+        }
+        
+        //since there are two ptrs left and right
+        //NULL all the left ptrs
+        current=root;
+        while(current){
+            current->left=NULL;
+            current=current->right;
+        }
+    }
+};
